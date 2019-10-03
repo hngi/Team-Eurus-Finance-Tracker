04488f9c378a0c6d050735e39bde0d071d3970b2
AideeX <54294692+AideeX@users.noreply.github.com>
2019-10-03 20:01:06 +0100

    Social login added

--- index.php
+++ index.php
@@ -18,6 +18,7 @@ if(!isset($_SESSION['ID'])){
     <meta charset="utf-8"/>
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="google-signin-client_id" content="954432276682-oc0iaf0uncrditdhj5d3dbbp2bcn0ns0.apps.googleusercontent.com">
     <title>Eurus Wallet | Welcome</title>
     <link rel="stylesheet" href="assets/css/app.css">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700,800&display=swap" rel="stylesheet">
@@ -31,9 +32,86 @@ if(!isset($_SESSION['ID'])){
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400&display=swap" rel="stylesheet">
     <script src="js/jquery.min.js"></script>
     <script src="js/backend_alert_controls.js"></script>
+    <script src="https://apis.google.com/js/platform.js" async defer></script>
+    <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v4.0&appId=542271436542069&autoLogAppEvents=1"></script>
 </head>
 <body>
 <!--Preloader-->
+
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '{542271436542069}',
+      cookie     : true,
+      xfbml      : true,
+      version    : '{v4.0}'
+    });
+      
+    FB.AppEvents.logPageView();   
+      
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
+   
+FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+});
+
+
+{
+    status: 'connected',
+    authResponse: {
+        accessToken: '...',
+        expiresIn:'...',
+        signedRequest:'...',
+        userID:'...'
+    }
+}
+
+<fb:login-button 
+  scope="public_profile,email"
+  onlogin="checkLoginState();">
+</fb:login-button>
+
+function checkLoginState() {
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+}
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId());
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail());
+}
+function onSuccess(googleUser) {
+      console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+    }
+    function onFailure(error) {
+      console.log(error);
+    }
+    function renderButton() {
+      gapi.signin2.render('my-signin2', {
+        'scope': 'profile email',
+        'width': 200,
+        'height': 50,
+        'longtitle': true,
+        'theme': 'dark',
+        'onsuccess': onSuccess,
+        'onfailure': onFailure
+      });
+    }
+</script>
+<div id="fb-root"></div>
+
 <div id="introLoader" class="introloader">
     <div class="loader-container">
         <div class="spinner">
@@ -154,6 +232,32 @@ if(!isset($_SESSION['ID'])){
                 <h6><i style="color: #DA381B; text-align: center; background-color: #EAF9EA;"><strong id="password-error"></strong></i></h6>
                 <h6><i style="color: #DA381B; text-align: center; background-color: #EAF9EA;"><strong id="failure"></strong></i></h6>
                 <!--- ALERTS --->
+            </div>
+            <div id="my-signin2">
+                <div style="height:50px;width:240px;" class="abcRioButton abcRioButtonBlue">
+                    <div class="abcRioButtonContentWrapper">
+                        <div class="abcRioButtonIcon" style="padding:15px">
+                            <div style="width:18px;height:18px;" class="abcRioButtonSvgImageWithFallback abcRioButtonIconImage abcRioButtonIconImage18">
+                                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="18px" height="18px" viewBox="0 0 48 48" class="abcRioButtonSvg">
+                                    <g>
+                                        <path fill="#EA4335" d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"></path>
+                                        <path fill="#4285F4" d="M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z"></path>
+                                        <path fill="#FBBC05" d="M10.53 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.97-6.19z"></path>
+                                        <path fill="#34A853" d="M24 48c6.48 0 11.93-2.13 15.89-5.81l-7.73-6c-2.15 1.45-4.92 2.3-8.16 2.3-6.26 0-11.57-4.22-13.47-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48z"></path>
+                                        <path fill="none" d="M0 0h48v48H0z"></path>
+                                    </g>
+                                </svg>
+                            </div>
+                        </div>
+                        <span style="font-size:16px;line-height:48px;" class="abcRioButtonContents">
+                            <span id="not_signed_inh9d86cjsnxs4">Continue with Google</span>
+                            <span id="connectedh9d86cjsnxs4" style="display:none">Continue with Google</span>
+                        </span>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="fb-login-button" data-width="20px" data-size="medium" data-button-type="continue_with" data-auto-logout-link="false" data-use-continue-as="false">
             </div>
             <div>
                 <button class="uk-button uk-button-theme-light" uk-toggle="target: #login">Login</button>
